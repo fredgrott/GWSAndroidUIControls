1f46eb1c91d3a3a23e72a4b238a1eefa020a45b9
Fred Grott <fred.grott@gmail.com>
2015-06-16 09:46:01 -0500

    added fadingactionbar  code

---
+++ gwsfadingactionbar/src/main/java/com/grottworkshop/gwsfadingactionbar/view/ObservableScrollView.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2013 Manuel Peinado
+ * Modifications Copyright 2105 Fred Grott(GrottWorkShop)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.grottworkshop.gwsfadingactionbar.view;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+/**
+ * @author Cyril Mottier with modifications from Manuel Peinado
+ * Created by fgrott on 6/16/2015.
+ */
+public class ObservableScrollView extends ScrollView implements IObservableScrollable {
+    // Edge-effects don't mix well with the translucent action bar in Android 2.X
+    private boolean mDisableEdgeEffects = true;
+
+    private IOnScrollChangedCallback mOnScrollChangedListener;
+
+    public ObservableScrollView(Context context) {
+        super(context);
+    }
+
+    public ObservableScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ObservableScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        super.onScrollChanged(l, t, oldl, oldt);
+        if (mOnScrollChangedListener != null) {
+            mOnScrollChangedListener.onScroll(l, t);
+        }
+    }
+
+    @Override
+    protected float getTopFadingEdgeStrength() {
+        // http://stackoverflow.com/a/6894270/244576
+        if (mDisableEdgeEffects && Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            return 0.0f;
+        }
+        return super.getTopFadingEdgeStrength();
+    }
+
+    @Override
+    protected float getBottomFadingEdgeStrength() {
+        // http://stackoverflow.com/a/6894270/244576
+        if (mDisableEdgeEffects && Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
+            return 0.0f;
+        }
+        return super.getBottomFadingEdgeStrength();
+    }
+
+    @Override
+    public void setOnScrollChangedCallback(IOnScrollChangedCallback callback) {
+        mOnScrollChangedListener = callback;
+    }
+}
